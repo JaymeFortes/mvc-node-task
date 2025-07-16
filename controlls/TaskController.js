d6a0b076e011ad3148f3611d8a4c4cb84166c8e0
JaymeFortes <jayme.fortes@edu.pucrs.br>
2025-07-16 14:23:42 -0300

    create task,edit task, delete task

--- controlls/TaskController.js
+++ controlls/TaskController.js
@@ -5,7 +5,53 @@ export default class TaskController {
     res.render('tasks/create');
   }
 
-  static showTasks(req, res) {
-    res.render('tasks/all');
+  static async createTaskSave(req, res) {
+    const tasks = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    };
+
+    await Task.create(tasks)
+      .then(res.redirect('/tasks'))
+      .catch((err) => console.log());
+  }
+
+  static async deleteTask(req, res) {
+    const id = req.body.id;
+
+    await Task.destroy({ where: { id: id } })
+      .then(res.redirect('/tasks'))
+      .catch((err) => console.log(err));
+  }
+
+  static async updateTask(req, res) {
+    const id = req.params.id;
+
+    const tasks = await Task.findOne({ where: { id: id }, raw: true });
+    res.render('tasks/edit', { task: tasks });
+  }
+
+  static async updateTaskSave(req, res) {
+    const id = req.body.id;
+
+    const tasks = {
+      title: req.body.title,
+      description: req.body.description,
+    };
+
+    try {
+      await Task.update(tasks, { where: { id: id } });
+      res.redirect('/tasks');
+    } catch (error) {
+      console.log('Error updating task:', error);
+      res.redirect('/tasks');
+    }
+  }
+
+  static async showTasks(req, res) {
+    const tasks = await Task.findAll({ raw: true });
+
+    res.render('tasks/all', { tasks });
   }
 }
--- router/taskRouter.js
+++ router/taskRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 import TaskController from '../controlls/TaskController.js';
 
 router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.createTaskSave);
+router.post('/delete', TaskController.deleteTask);
+router.get('/edit/:id', TaskController.updateTask);
+router.post('/edit', TaskController.updateTaskSave);
 router.get('/', TaskController.showTasks);
 
 export default router;
-
